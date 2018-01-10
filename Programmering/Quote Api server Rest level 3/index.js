876490de3a32febd9719660b8d2295a80b0b3876
Lassemeldgaard <Lassemeldgaard@hotmail.dk>
2018-01-10 13:25:51 +0100

    api level 3 mangler doc

--- Programmering/Quote Api server Rest level 3/index.js
+++ Programmering/Quote Api server Rest level 3/index.js
@@ -11,26 +11,36 @@ app.use(function (req, resp, next) {
     resp.header("Access-Control-Allow-Origin", "*");
     resp.header("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
     resp.header("Access-Control-Allow-Headers", "Origin, X-Requested-with,Content-type, Accept");
+    resp.header("Content-Type", "application/hal+json");
     next();
 });
 var quotes = [{
-        title: "Awesome quote",
+        title: "Newest quote",
         description: 'This is an awesome quote made by lasse',
         rating: 2,
         date: new Date("Wed Sep 20 2017")
     },
     {
-        title: "Awesome quote2",
+        title: "Highest rated quote",
         description: 'This is an awesome quote 2 made by lasse',
         rating: 3,
         date: new Date("2017-09-18")
     },
     {
-        title: "Awesome quote3",
+        title: "Lowest rating mid created",
         description: 'This is an awesome quote 2 made by lasse',
         rating: 1,
         date: new Date("2017-09-19")
     }];
+/*http://localhost:3000/   Dette er den url der skal bruge under Explorer i Halbrowseren http://api.m.ox.ac.uk/browser (http://api.m.ox.ac.uk/browser/#http://localhost:3000/)*/
+app.get("/", function (req, resp) {
+    var res = new hal_1.Resource({}, "http://localhost:3000/");
+    res.link("AddQuotes: ", "http://localhost:3000/quotes/create");
+    res.link("getSpecificQuote: ", "http://localhost:3000/quotes/list");
+    res.link("Highest rated: ", "http://localhost:3000/quotes/highestrated");
+    res.link("Newest quotes: ", "http://localhost:3000/quotes/newest");
+    resp.status(HTTP.OK).json(res);
+});
 //ET endpoint for hver resource
 app.put("/quotes/:id/downvote", function (req, resp) {
     var result;
@@ -53,57 +63,55 @@ app.delete("/quotes/:id/", function (req, resp) {
     result = "Deleted";
     resp.status(HTTP.NO_CONTENT).send(result);
 });
-app.post("/quotes", function (req, resp) {
+app.post("/quotes/create", function (req, resp) {
     var title = req.body.title;
     var description = req.body.description;
     var rating = 0;
     var date = new Date();
     quotes.push({ title: title, description: description, rating: rating, date: date });
-    var result = "added";
+    var result = "created";
     resp.status(HTTP.CREATED).send(result);
 });
 app.get("/quotes/list", function (req, resp) {
-    var result = new hal_1.Resource({}, "/quotes/list");
+    var result = new hal_1.Resource({}, "http://localhost:3000/quotes/list");
     for (var i = 0; i < quotes.length; i++) {
-        var title = quotes[i].title;
-        var quote = new hal_1.Resource({ title: title }, "/quotes/" + i);
-        result.link(quotes[i].title, "/quotes/" + i);
-        result.embed("quotes", quote);
+        result.link(quotes[i].title, "http://localhost:3000/quotes/" + i);
+        result.embed("quotes", quotes[i]);
     }
     resp.status(HTTP.OK).json(result);
 });
 app.get("/quotes/highestrated", function (req, resp) {
-    var result = new hal_1.Resource({}, "/quotes/highestrated");
+    var result = new hal_1.Resource({}, "http://localhost:3000/quotes/highestrated");
     quotes.sort(function (a, b) { return b.rating > a.rating ? 1 : -1; });
     for (var i = 0; i < quotes.length; i++) {
         var title = quotes[i].title;
         var description = quotes[i].description;
         var rating = quotes[i].rating;
         var date = quotes[i].date;
-        var quote = new hal_1.Resource({ title: title, description: description, rating: rating, date: date }, "/quotes/" + i);
-        result.link(quotes[i].title, "/quotes/" + i);
-        result.embed("quotes", quote);
+        result.link(quotes[i].title, "http://localhost:3000/quotes/" + i);
+        result.embed("quotes", quotes[i]);
     }
     resp.status(HTTP.OK).send(result);
 });
 app.get("/quotes/newest", function (req, resp) {
-    var result = new hal_1.Resource({}, "/quotes/newest");
+    var result = new hal_1.Resource({}, "http://localhost:3000/quotes/newest");
     quotes.sort(function (a, b) { return b.date - a.date; });
     for (var i = 0; i < quotes.length; i++) {
         var title = quotes[i].title;
         var description = quotes[i].description;
         var rating = quotes[i].rating;
         var date = quotes[i].date;
-        var quote = new hal_1.Resource({ title: title, description: description, rating: rating, date: date }, "/quotes/" + i);
-        result.link(quotes[i].title, "/quotes/" + i);
-        result.embed("quotes", quote);
+        result.link(quotes[i].title, "http://localhost:3000/quotes/" + i);
+        result.embed("quotes", quotes[i]);
     }
     resp.status(HTTP.OK).send(result);
 });
 app.get("/quotes/:id", function (req, resp) {
     var id = req.params.id;
-    var result = new hal_1.Resource(quotes[req.params], "/quotes/" + id);
-    result.link("Quote: " + id, "/quotes/" + id);
+    var result = new hal_1.Resource(quotes[id], "/quotes/" + id);
+    result.link("Quote: " + "Delete quote", "http://localhost:3000//quotes/:id/");
+    result.link("Quote: " + "Upvote quote", "http://localhost:3000//quotes/:id/upvote");
+    result.link("Quote: " + "Downvote quote", "http://localhost:3000///quotes/:id/downvote");
     result.embed("quotes", quotes[id]);
     resp.status(HTTP.OK).send(result);
 });
--- Programmering/Quote Api server Rest level 3/index.ts
+++ Programmering/Quote Api server Rest level 3/index.ts
@@ -13,29 +13,39 @@ app.use((req, resp, next) => {
     resp.header("Access-Control-Allow-Origin", "*");
     resp.header("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS")
     resp.header("Access-Control-Allow-Headers", "Origin, X-Requested-with,Content-type, Accept");
-    
+    resp.header("Content-Type", "application/hal+json");
     next();
 });
 
 let quotes: any[] = [{
-    title: "Awesome quote",
+    title: "Newest quote",
     description: 'This is an awesome quote made by lasse',
     rating: 2,
     date: new Date("Wed Sep 20 2017")
 },
 {
-    title: "Awesome quote2",
+    title: "Highest rated quote",
     description: 'This is an awesome quote 2 made by lasse',
     rating: 3,
     date: new Date("2017-09-18")
 },
 {
-    title: "Awesome quote3",
+    title: "Lowest rating mid created",
     description: 'This is an awesome quote 2 made by lasse',
     rating: 1,
     date: new Date("2017-09-19")
 }];
+/*http://localhost:3000/   Dette er den url der skal bruge under Explorer i Halbrowseren http://api.m.ox.ac.uk/browser (http://api.m.ox.ac.uk/browser/#http://localhost:3000/)*/
 
+app.get("/", (req, resp) =>{
+    let res = new Resource({}, "http://localhost:3000/");
+    res.link("AddQuotes: ", "http://localhost:3000/quotes/create")
+    res.link("getSpecificQuote: ", "http://localhost:3000/quotes/list")
+    res.link("Highest rated: ", "http://localhost:3000/quotes/highestrated")
+    res.link("Newest quotes: ", "http://localhost:3000/quotes/newest")
+
+    resp.status(HTTP.OK).json(res);
+})
 
 //ET endpoint for hver resource
 app.put("/quotes/:id/downvote", (req, resp) => {
@@ -60,67 +70,59 @@ app.delete("/quotes/:id/", (req, resp) => {
 
     resp.status(HTTP.NO_CONTENT).send(result);
 });
-app.post("/quotes", (req, resp) => {
+app.post("/quotes/create", (req, resp) => {
     let title = req.body.title;
     let description = req.body.description;
     let rating = 0;
     let date = new Date();
     quotes.push({ title, description, rating, date });
-    let result = "added";
+    let result = "created"
     resp.status(HTTP.CREATED).send(result);
 });
 app.get("/quotes/list", (req, resp) => {
-    let result = new Resource({}, "/quotes/list")
-    
+    let result = new Resource({}, "http://localhost:3000/quotes/list")
+
     for (let i = 0; i < quotes.length; i++) {
-        let title = quotes[i].title
-        let quote = new Resource({title}, "/quotes/" + i);
-        result.link(quotes[i].title, "/quotes/" + i)
-        
-        result.embed("quotes", quote)
+        result.link(quotes[i].title, "http://localhost:3000/quotes/" + i);
+        result.embed("quotes", quotes[i])
     }
     resp.status(HTTP.OK).json(result);
 });
-
-
-
 app.get("/quotes/highestrated", (req, resp) => {
-    let result = new Resource({}, "/quotes/highestrated")
+    let result = new Resource({}, "http://localhost:3000/quotes/highestrated")
     quotes.sort((a, b) => b.rating > a.rating ? 1 : -1)
     for (let i = 0; i < quotes.length; i++) {
         let title = quotes[i].title
         let description = quotes[i].description
         let rating = quotes[i].rating
         let date = quotes[i].date
-
-        let quote = new Resource({title, description, rating, date}, "/quotes/" + i);
         
-        result.link(quotes[i].title, "/quotes/" + i)        
-        result.embed("quotes", quote)
+        result.link(quotes[i].title, "http://localhost:3000/quotes/" + i)        
+        result.embed("quotes", quotes[i])
     }
     resp.status(HTTP.OK).send(result)
 });
 app.get("/quotes/newest", (req, resp) => {
-    let result = new Resource({}, "/quotes/newest")
+    let result = new Resource({}, "http://localhost:3000/quotes/newest")
     quotes.sort((a, b) => b.date - a.date)
     for (let i = 0; i < quotes.length; i++) {
         let title = quotes[i].title
         let description = quotes[i].description
         let rating = quotes[i].rating
         let date = quotes[i].date
-
-        let quote = new Resource({title, description, rating, date}, "/quotes/" + i);
         
-        result.link(quotes[i].title, "/quotes/" + i)        
-        result.embed("quotes", quote)
+        result.link(quotes[i].title, "http://localhost:3000/quotes/" + i)        
+        result.embed("quotes", quotes[i])
     }
     resp.status(HTTP.OK).send(result)
 });
 app.get("/quotes/:id", (req, resp) => {
-    
+
     let id = req.params.id;
-    let result = new Resource(quotes[req.params], "/quotes/" + id) ;
-    result.link("Quote: " + id,"/quotes/" + id )
+    let result = new Resource(quotes[id], "/quotes/" + id) ;
+    result.link("Quote: " + "Delete quote", "http://localhost:3000//quotes/:id/" )
+    result.link("Quote: " + "Upvote quote", "http://localhost:3000//quotes/:id/upvote" )
+    result.link("Quote: " + "Downvote quote", "http://localhost:3000///quotes/:id/downvote" )
     result.embed("quotes", quotes[id])
     resp.status(HTTP.OK).send(result)
 });
